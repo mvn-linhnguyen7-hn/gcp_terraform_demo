138f9c412cf92467d1b2adfe53c50d1067e429f1
mvn-linhnguyen7-hn <linh.nguyen7@monstar-lab.com>
2022-09-16 09:10:57 +0700

    update main

--- gcp_funtions/pubsub.tf
+++
@@ -1,3 +0,0 @@
-resource "google_pubsub_topic" "test_function" {
-  name = "${var.project_id}-test-topic"
-}
--- gcp_funtions/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  type = string
-  default = "terraform-gcp-361801"
-}
-
-variable "region" {
-  type = string
-  default = "asia-northeast1"
-}
